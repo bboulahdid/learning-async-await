64908a59ebf8c59e0132c048a11252b7713719f3
Boubker BOULAHDID <boulahdid.boubker@gmail.com>
2017-05-04 00:33:08 +0200

    Add async/await

--- async.js
+++ async.js
@@ -10,4 +10,13 @@ const showGithubUserInfos = (username) => {
     })
 };
 
-showGithubUserInfos('bboulahdid');
+async function showGithubUserInfosAsync(username) {
+  const url = `https://api.github.com/users/${username}`;
+  const response = await fetch(url);
+  const user = await response.json();
+
+  console.log(user.name);
+  console.log(user.location);
+};
+
+showGithubUserInfosAsync('bboulahdid');
